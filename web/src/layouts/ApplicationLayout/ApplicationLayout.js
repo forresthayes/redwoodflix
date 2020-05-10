57495d40273df0cd9c1b52b1fa9db8b90267a684
Forrest Hayes <forresthayes@pm.me>
2020-05-10 13:40:21 -0500

    Style navbar in layout

--- web/src/layouts/ApplicationLayout/ApplicationLayout.js
+++ web/src/layouts/ApplicationLayout/ApplicationLayout.js
@@ -1,3 +1,4 @@
+import { Link, routes } from '@redwoodjs/router'
 import { Navbar, Section, Container, Title } from 'rbx'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFilm } from '@fortawesome/free-solid-svg-icons'
@@ -5,14 +6,27 @@ import { faFilm } from '@fortawesome/free-solid-svg-icons'
 const ApplicationLayout = ({ children }) => {
   return (
     <>
-      <Navbar color="link">
+      <Navbar color="info">
         <Navbar.Brand>
-          <Navbar.Item>
-            <Title size={1} className="has-text-light">
+          <Navbar.Item as={Link} to={routes.home()}>
+            <Title size={1} textColor="light">
               <FontAwesomeIcon icon={faFilm} /> FLIX
             </Title>
           </Navbar.Item>
         </Navbar.Brand>
+        <Navbar.Menu>
+          <Navbar.Segment align="start">
+            <Navbar.Item
+              as={Link}
+              to={routes.home()}
+              textColor="light"
+              textSize={5}
+              style={{ paddingTop: '1.9rem', marginLeft: '2rem' }}
+            >
+              All Movies
+            </Navbar.Item>
+          </Navbar.Segment>
+        </Navbar.Menu>
       </Navbar>
       <Container>
         <Section>{children}</Section>
