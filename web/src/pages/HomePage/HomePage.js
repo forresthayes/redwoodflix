d1c7d15d84fd28897ea5ab6cd9cb3163fc190ceb
Forrest Hayes <forresthayes@pm.me>
2020-05-10 17:51:38 -0500

    Omit Content component from homepage

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,13 +1,10 @@
 import ApplicationLayout from 'src/layouts/ApplicationLayout'
-import { Content } from 'rbx'
 import FlixCell from 'src/components/FlixCell'
 
 const HomePage = () => {
   return (
     <ApplicationLayout>
-      <Content>
-        <FlixCell />
-      </Content>
+      <FlixCell />
     </ApplicationLayout>
   )
 }
