35b5e95595ce400a2c7c7a49cb06723694d5ed9e
Forrest Hayes <forresthayes@pm.me>
2020-05-12 11:26:16 -0500

    Build Review form

--- web/src/pages/NewReviewPage/NewReviewPage.js
+++ web/src/pages/NewReviewPage/NewReviewPage.js
@@ -1,9 +1,96 @@
-const NewReviewPage = () => {
+import {
+  Label,
+  Form,
+  TextField,
+  TextAreaField,
+  Submit,
+  FieldError,
+} from '@redwoodjs/web'
+import { useState } from 'react'
+import ApplicationLayout from 'src/layouts/ApplicationLayout'
+import { Link, routes } from '@redwoodjs/router'
+import { Rating } from '@material-ui/lab'
+import { Title, Section, Container, Field, Control, Column } from 'rbx'
+
+const NewReviewPage = ({ id }) => {
+  const [starValue, setStarValue] = useState(3)
+  const onSubmit = (data) => {
+    console.log({ stars: starValue, ...data })
+  }
+
   return (
-    <div>
-      <h1>NewReviewPage</h1>
-      <p>Find me in ./web/src/pages/NewReviewPage/NewReviewPage.js</p>
-    </div>
+    <ApplicationLayout>
+      <Section>
+        <Container>
+          <Column.Group>
+            <Column size="4" offset="1">
+              <Title>
+                New Review for{' '}
+                <Link to={routes.flick({ id: id })}>a movie</Link>
+              </Title>
+              <Form onSubmit={onSubmit}>
+                <Field>
+                  <Label name="name" className="label" errorClassName="error">
+                    Name
+                  </Label>
+                  <Control>
+                    <TextField
+                      name="name"
+                      className="input"
+                      validation={{ required: true }}
+                      errorClassName="input is-danger"
+                    />
+                    <FieldError name="name" className="has-text-danger" />
+                  </Control>
+                </Field>
+
+                <Field>
+                  <Label name="stars" className="label">
+                    Stars
+                  </Label>
+                  <Control>
+                    <Rating
+                      name="stars"
+                      value={starValue}
+                      onChange={(event, newStarValue) =>
+                        setStarValue(newStarValue)
+                      }
+                    />
+                  </Control>
+                </Field>
+
+                <Field>
+                  <Label
+                    name="comment"
+                    className="label"
+                    errorClassName="error"
+                  >
+                    Comment
+                  </Label>
+                  <Control>
+                    <TextAreaField
+                      name="comment"
+                      placeholder="What did you think?"
+                      className="textarea"
+                      errorClassName="input is-danger"
+                      validation={{
+                        required: true,
+                        minLength: 4,
+                      }}
+                    />
+                    <FieldError name="comment" className="has-text-danger" />
+                  </Control>
+                </Field>
+
+                <Control>
+                  <Submit className="button is-link">Post Review</Submit>
+                </Control>
+              </Form>
+            </Column>
+          </Column.Group>
+        </Container>
+      </Section>
+    </ApplicationLayout>
   )
 }
 
